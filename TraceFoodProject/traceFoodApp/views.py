cf8373f7ad71cfbdb44ba4cae26ead29d95f8a04
Valeria Guerra <vguerraz@eafit.edu.co>
2022-09-12 12:39:26 -0500

    Correccion error views.py

--- TraceFoodProject/traceFoodApp/views.py
+++ TraceFoodProject/traceFoodApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from pymongo import MongoClient
-client = pymongo.MongoClient('mongodb+srv://vguerraz:TwD374X@cluster0.ieqcc.mongodb.net/test')
+client = MongoClient('mongodb+srv://vguerraz:TwD374X@cluster0.ieqcc.mongodb.net/test')
 db = client['TraceFood']
 
 # Create your views here.
